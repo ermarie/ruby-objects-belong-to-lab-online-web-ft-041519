216895d636fc59403146e975822219281e41d6d3
Erin Stalzer <erinstalzer@gmail.com>
2019-04-30 22:13:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,13 @@ class Artist
 
   attr_accessor :name
 
+<<<<<<< HEAD
   def initialize
 
+=======
+  def initialize(name)
+    @name = name
+>>>>>>> 5ea44e30fdc21038e61e39f448e4ebcdc0bb4eec
   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,10 @@
 class Song
 
+<<<<<<< HEAD
   attr_accessor :title, :artist
+=======
+  attr_accessor :title
+>>>>>>> 5ea44e30fdc21038e61e39f448e4ebcdc0bb4eec
 
   def initialize
 
